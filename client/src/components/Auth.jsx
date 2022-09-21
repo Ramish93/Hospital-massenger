9d4e63c80d67b361a7d889c6e03c2281b099270e
Ramish Hassan <ramishshah93@gmail.com>
2022-09-21 16:48:54 +0200

    getting user info from cookies in front end and showing UI according to it in app.jsx

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 import signinImage from '../assets/signup.jpg'
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName:'',
     username:'',
@@ -31,6 +33,18 @@ const Auth = () => {
         const {data: {token, userId, hashedPassword} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, avatarURL, fullName, phoneNumber
         })
+
+        cookies.set(`token`, token)
+        cookies.set(`username`, username)
+        cookies.set(`fullName`, fullName)
+        cookies.set(`userId`, userId)
+
+        if(isSignup){
+            cookies.set(`phoneNumber`, phoneNumber)
+            cookies.set(`avatarURL`, avatarURL)
+            cookies.set(`hashedPassword`, hashedPassword)
+        }
+        window.location.reload();
     }
 
     const switchMode = () => {
