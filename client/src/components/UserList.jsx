98ec33556e4e31718bacdf54ac0bd1439e8b129e
Ramish Hassan <ramishshah93@gmail.com>
2022-09-22 16:04:05 +0200

    added some jsx and react component inside userlist.jsx

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,11 +1,55 @@
 import React,{useEffect, useState} from 'react'
-import { Avatar, useChatChannel } from 'stream-chat-react'
+import { Avatar, useChatContext } from 'stream-chat-react'
 
 import { InviteIcon } from '../assets'
 
+const ListContainer = ({children}) => {
+    return (
+        <div className='user-list__container'>
+            <div className='user-list__header'>
+                <p>User</p>
+                <p>Invite</p>
+            </div>
+        </div>
+    )
+}
+
+const userItem = () => {
+    return (
+        <div className='user-item__wrapper'>
+            <div className='user-item_name__wrapper'>
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
 const UserList = () => {
+    const {client} = useChatContext()
+     const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+      const getUser = async () => {
+        if(loading) return
+      
+      setLoading(true)
+        const response = await client.queryUsers({
+
+        })
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+    }, [filters])
+    
+
   return (
-    <div>UserList</div>
+    <ListContainer>
+        userlist
+    </ListContainer>
   )
 }
 
