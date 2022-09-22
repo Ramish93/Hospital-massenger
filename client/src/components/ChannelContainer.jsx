89f60866382f604633a81f8701f31a1e2db61c93
Ramish Hassan <ramishshah93@gmail.com>
2022-09-22 10:37:25 +0200

    added states in app.jsx for creating, editing channels, using them in channelContainer component.

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,6 +1,39 @@
 import React from 'react'
+import {Channel, useChatContext} from 'stream-chat-react'
 
-const ChannelContainer = () => {
+import {ChannelInner, CreateChannel, EditChannel, TeamChannel} from './'
+
+const ChannelContainer = ({
+  isCreating,
+  setIsCrearing,
+  isEditing,
+  setIsEditing,
+  createType
+}) => {
+
+  const {channel} = useChatContext();
+
+  if(isCreating){
+    return (
+      <div className='channel__container'>
+        <CreateChannel createType={createType} setIsCrearing={setIsCrearing} />
+      </div>
+    )
+  }
+  if(isEditing){
+    return (
+      <div className='channel__container'>
+      <EditChannel setIsEditing={setIsEditing} />
+    </div>
+    )
+  }
+
+  const EmptyState=() => (
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'> This is the beganing of your chat history</p>
+    </div>
+  )
+
   return (
     <div>ChannelContainer</div>
   )
